2a159ad8432ad3d576b0ecb412c5253c96d13f82
SaurabhJaybhaye <saurabhjaybhaye01@gmail.com>
2026-02-01 19:54:57 +0530

    feat: Configure project for publishing as a React component library, including build setup, package metadata, and documentation.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...dts({
+        tsconfigPath: './tsconfig.app.json',
+        insertTypesEntry: true,
+      }),
+      apply: 'build',
+    },
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'ProjectSB',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
   test: {
